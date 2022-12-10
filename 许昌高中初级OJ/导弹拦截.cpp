c1df1699407cc4b53a4a608e591c812ca79ebd34
doubleki7461 <114803508+doubleki7461@users.noreply.github.com>
2022-12-10 16:01:11 +0800

    更新 导弹拦截.cpp

--- 许昌高中初级OJ/导弹拦截.cpp
+++ 许昌高中初级OJ/导弹拦截.cpp
@@ -1,43 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1000;
+const int MAXN=100000;
 int h[MAXN+5];
-int dd[MAXN+5];
+int ans1=1;
+int ans2=1;
+int bomb[MAXN+5];
+int dp[MAXN+5];
+int vis[MAXN+5];
+int solve(int k)
+{
+	if(vis[k])
+	{
+		return dp[k];
+	}
+	else
+	{
+		for(int i=1;i<k;i++)
+		{
+			if(bomb[k]<=bomb[i])
+			{
+				dp[k]=max(solve(i)+1,dp[k]);
+			}
+		}
+	}
+	vis[k]=1;
+	return dp[k];
+}
 int main()
 {
-	int ans=1;
-	int num=0;
-	cin>>dd[1];
-	h[1]=dd[1];
-	int i=2;
-	while(cin>>dd[i])
+	memset(vis,0,sizeof(vis));
+	int n=1;
+	cin>>bomb[1];
+	h[1]=bomb[1];
+	vis[1]=1;
+	while(cin>>bomb[n+1])
 	{
-		num++;
-		sort(h+1,h+ans+1);
+		n++;
+		sort(h+1,h+ans2+1);
 		int temp=1;
-		while(dd[i]>h[temp])
+		while(bomb[n]>h[temp])
 		{
 			temp++;
-			if(temp>ans)
+			if(temp>ans2)
 			{
-				ans++;
-				h[ans]=dd[i];
+				ans2++;
+				h[ans2]=bomb[n];
 				break;
 			}
 		}
-		if(temp<=ans)
+		if(temp<=ans2)
 		{
-			h[temp]=dd[i];
+			h[temp]=bomb[n];
 		}
 	}
-//	int res=1,a=dd[1];
-//	for(int j=2;j<=num;j++)
-//	{
-//		if(dd[j]<a)
-//		{
-//			a=dd[j];
-//			res++;
-//		}
-//	}
-	cout<<0<<" "<<ans;
+	for(int i=1;i<=n;i++)
+	{
+		dp[i]=1;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		ans1=max(ans1,solve(i));
+	}
+	cout<<ans1<<endl<<ans2<<endl;
 }
