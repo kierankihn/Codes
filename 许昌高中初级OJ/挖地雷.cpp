6b14439a391816e104f2a75d12ecd84de188507b
doubleki7461 <114803508+doubleki7461@users.noreply.github.com>
2022-12-17 20:57:53 +0800

    更改了编码格式

--- 许昌高中初级OJ/挖地雷.cpp
+++ 许昌高中初级OJ/挖地雷.cpp
@@ -1,72 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=200;
+const int MAXN = 200;
 int n;
-int res=-1;
-int mp[MAXN+5][MAXN+5];
-int dp[MAXN+5][3];  //0为原数据 1为dp 2为上一个点 
+int res = -1;
+int mp[MAXN + 5][MAXN + 5];
+int dp[MAXN + 5][3]; // 0涓哄師鏁版嵁 1涓篸p 2涓轰笂涓�涓偣
 int solve(int k)
 {
-	int ans=dp[k][0];
-	if(dp[k][1]!=-1)
+	int ans = dp[k][0];
+	if (dp[k][1] != -1)
 	{
 		return dp[k][1];
 	}
 	else
 	{
-		for(int i=k;i<=n;i++)
+		for (int i = k; i <= n; i++)
 		{
-			if(mp[k][i])
+			if (mp[k][i])
 			{
-				if(ans<solve(i)+dp[k][0])
+				if (ans < solve(i) + dp[k][0])
 				{
-					ans=solve(i)+dp[k][0];
-					dp[k][2]=i;
+					ans = solve(i) + dp[k][0];
+					dp[k][2] = i;
 				}
 			}
 		}
 	}
-	return dp[k][1]=ans;
+	return dp[k][1] = ans;
 }
 void print_ans(int k)
 {
-	cout<<k;
-	if(dp[k][2]!=-1)
+	cout << k;
+	if (dp[k][2] != -1)
 	{
-		cout<<"-";
+		cout << "-";
 		print_ans(dp[k][2]);
 	}
 }
 int main()
 {
-	memset(mp,0,sizeof(mp));
-	cin>>n;
-	for(int i=1;i<=n;i++)
+	memset(mp, 0, sizeof(mp));
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
-		cin>>dp[i][0];
-		dp[i][1]=-1;
-		dp[i][2]=-1;
+		cin >> dp[i][0];
+		dp[i][1] = -1;
+		dp[i][2] = -1;
 	}
-	int tmp1,tmp2;
-	while(cin>>tmp1>>tmp2)
+	int tmp1, tmp2;
+	while (cin >> tmp1 >> tmp2)
 	{
-		if((!tmp1)||(!tmp2))
+		if ((!tmp1) || (!tmp2))
 		{
 			break;
 		}
-		mp[tmp1][tmp2]=1;
+		mp[tmp1][tmp2] = 1;
 	}
 	int p;
-	for(int i=1;i<=n;i++)
+	for (int i = 1; i <= n; i++)
 	{
-		if(solve(i)>res)
+		if (solve(i) > res)
 		{
-			res=solve(i);
-			p=i;
+			res = solve(i);
+			p = i;
 		}
 	}
 	print_ans(p);
-	cout<<endl;
-	cout<<res<<endl;
+	cout << endl;
+	cout << res << endl;
 	return 0;
 }
