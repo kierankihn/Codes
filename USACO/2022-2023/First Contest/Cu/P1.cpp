ff36dfed559cb05599113074598573639c9d8e99
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-01-06 05:41:23 +0000

    格式化了 P1.cpp

--- USACO/2022-2023/First Contest/Cu/P1.cpp
+++ USACO/2022-2023/First Contest/Cu/P1.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1e5;
+const int MAXN = 1e5;
 int n;
-long long cost[MAXN+5];
-long long ans=-1;
+long long cost[MAXN + 5];
+long long ans = -1;
 long long p;
 int main()
 {
-	cin>>n;
-	for(int i=1;i<=n;i++)
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
-		cin>>cost[i];
+		cin >> cost[i];
 	}
-	sort(cost+1,cost+n+1);
-	for(int i=1;i<=n;i++)
+	sort(cost + 1, cost + n + 1);
+	for (int i = 1; i <= n; i++)
 	{
-		if((n-i+1)*cost[i]>ans)
+		if ((n - i + 1) * cost[i] > ans)
 		{
-			ans=cost[i]*(n-i+1);
-			p=cost[i];
+			ans = cost[i] * (n - i + 1);
+			p = cost[i];
 		}
 	}
-	cout<<ans<<" "<<p<<endl;
-	
+	cout << ans << " " << p << endl;
+
 	return 0;
 }
