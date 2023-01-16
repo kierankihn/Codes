04206b674a7b526154196d5ba015003a097f2cda
doubleki7461 <114803508+doubleki7461@users.noreply.github.com>
2023-01-16 07:16:33 +0000

    格式化了 N^N.cpp

--- 许昌高中初级OJ/N^N.cpp
+++ 许昌高中初级OJ/N^N.cpp
@@ -5,17 +5,17 @@ int solve(int n)
 	int ans;
 	double nlgn;
 	int nlgn_t;
-	nlgn=log10(n)*n;
-	nlgn_t=(int)nlgn;
-	ans=pow(10,nlgn-nlgn_t);
+	nlgn = log10(n) * n;
+	nlgn_t = (int)nlgn;
+	ans = pow(10, nlgn - nlgn_t);
 	return ans;
 }
 int main()
 {
 	int n;
-	while(cin>>n)
+	while (cin >> n)
 	{
-		cout<<solve(n)<<endl;
+		cout << solve(n) << endl;
 	}
 	return 0;
 }
