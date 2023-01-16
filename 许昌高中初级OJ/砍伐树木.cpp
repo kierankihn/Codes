9d8429b9a26dff820b3bfa0a77652e1a04dc77be
doubleki7461 <114803508+doubleki7461@users.noreply.github.com>
2023-01-16 01:03:44 +0000

    格式化了 砍伐树木.cpp

--- 许昌高中初级OJ/砍伐树木.cpp
+++ 许昌高中初级OJ/砍伐树木.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN=1e6;
+const int MAXN = 1e6;
 int n;
 long long m;
-int tree[MAXN+5];
+int tree[MAXN + 5];
 long long sum(int k)
 {
-	long long res=0;
-	for(int i=1;i<=n;i++)
+	long long res = 0;
+	for (int i = 1; i <= n; i++)
 	{
-		if(tree[i]>k)
+		if (tree[i] > k)
 		{
-			res+=tree[i]-k;
+			res += tree[i] - k;
 		}
 	}
 	return res;
 }
 int main()
 {
-	cin>>n>>m;
-	int maxt=-1;
-	for(int i=1;i<=n;i++)
+	cin >> n >> m;
+	int maxt = -1;
+	for (int i = 1; i <= n; i++)
 	{
-		cin>>tree[i];
-		if(tree[i]>maxt)
+		cin >> tree[i];
+		if (tree[i] > maxt)
 		{
-			maxt=tree[i];
+			maxt = tree[i];
 		}
 	}
-	int l=0,r=maxt;
+	int l = 0, r = maxt;
 	int mid;
 	int ans;
-	while(/*l<r*/l<=r)
+	while (l <= r)
 	{
-		mid=(l+r)/2;
-		long long s=sum(mid);
-		if(s<m)
+		mid = (l + r) / 2;
+		long long s = sum(mid);
+		if (s < m)
 		{
-//			r=mid-1;
-			r=mid-1;
+			r = mid - 1;
 		}
 		else
 		{
-//			l=mid;
-			l=mid+1;
-			ans=mid;
+			l = mid + 1;
+			ans = mid;
 		}
 	}
-	cout<</*l*/ans<<endl;
+	cout << ans << endl;
 	return 0;
 }
