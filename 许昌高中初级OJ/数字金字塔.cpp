70334659926e96e16cbd711fbc364a2b864965c4
doubleki7461 <114803508+doubleki7461@users.noreply.github.com>
2023-01-27 07:41:48 +0000

    格式化了代码

--- 许昌高中初级OJ/数字金字塔.cpp
+++ 许昌高中初级OJ/数字金字塔.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXR=1000;
+const int MAXR = 1000;
 int r;
-int nums[MAXR+5][MAXR+5];
-int dp[MAXR+5][MAXR+5];
-int solve(int i,int j)
+int nums[MAXR + 5][MAXR + 5];
+int dp[MAXR + 5][MAXR + 5];
+int solve(int i, int j)
 {
-	if(dp[i][j]!=-1)
+	if (dp[i][j] != -1)
 	{
 		return dp[i][j];
 	}
-	return dp[i][j]=max(solve(i+1,j),solve(i+1,j+1))+nums[i][j];
+	return dp[i][j] = max(solve(i + 1, j), solve(i + 1, j + 1)) + nums[i][j];
 }
 int main()
 {
-	memset(dp,-1,sizeof(dp));
-	cin>>r;
-	for(int i=1;i<=r;i++)
+	memset(dp, -1, sizeof(dp));
+	cin >> r;
+	for (int i = 1; i <= r; i++)
 	{
-		for(int j=1;j<=i;j++)
+		for (int j = 1; j <= i; j++)
 		{
-			cin>>nums[i][j];
+			cin >> nums[i][j];
 		}
 	}
-	for(int i=1;i<=r;i++)
+	for (int i = 1; i <= r; i++)
 	{
-		dp[r][i]=nums[r][i];
+		dp[r][i] = nums[r][i];
 	}
-	cout<<solve(1,1)<<endl;
-	
+	cout << solve(1, 1) << endl;
+
 	return 0;
 }
