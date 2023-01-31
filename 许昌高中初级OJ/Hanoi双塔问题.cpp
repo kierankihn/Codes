c34a327530933039f4a03390ff6da67d60b3ce21
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-01-31 07:59:18 +0000

    更新了  Hanoi双塔问题.cpp

--- 许昌高中初级OJ/Hanoi双塔问题.cpp
+++ 许昌高中初级OJ/Hanoi双塔问题.cpp
@@ -23,45 +23,11 @@ void print(unsigned __int128 a)
 }
 int main()
 {
-    // vis[0] = 1;
-    // int n;
-    // cin >> n;
-    // // cout << h(n) << endl;
-    // print(h(n));
-    // cout << endl;
-    // return 0;
+    vis[0] = 1;
     int n;
-    int ans[210];
-    int j;
     cin >> n;
-    memset(ans, 0, sizeof(ans));
-    for (int i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= 200; j++)
-            ans[j] = ans[j] * 2;
-        ans[1]++;
-        for (j = 1; j <= 200; j++)
-            if (ans[j] >= 10)
-            {
-                ans[j + 1] += ans[j] / 10;
-                ans[j] = ans[j] % 10;
-            }
-    }
-    for (j = 1; j <= 200; j++)
-        ans[j] = ans[j] * 2;
-    for (j = 1; j <= 200; j++)
-        if (ans[j] >= 10)
-        {
-            ans[j + 1] += ans[j] / 10;
-            ans[j] = ans[j] % 10;
-        }
-    bool p = 0;
-    for (int i = 200; i >= 1; i--)
-    {
-        if (ans[i] != 0)
-            p = 1;
-        if (p)
-            printf("%d", ans[i]);
-    }
+    // cout << h(n) << endl;
+    print(h(n));
     cout << endl;
+    return 0;
 }
