f2fc5d522f49877649c4477b11b2ce4021533c6e
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-02-25 08:54:13 +0800

    创建了 二叉树输出.cpp

---
+++ 许昌高中初级OJ/合并果子.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 114514;
+int n;
+int ans = 0;
+priority_queue<int,vector<int>,greater<int> > nums;
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        nums.push(tmp);
+    }
+    while (nums.size() != 1)
+    {
+        int tmp1, tmp2;
+        tmp1 = nums.top();
+        nums.pop();
+        tmp2 = nums.top();
+        nums.pop();
+        nums.push(tmp1 + tmp2);
+        ans += tmp1 + tmp2;
+    }
+    cout << ans << endl;
+    return 0;
+}
