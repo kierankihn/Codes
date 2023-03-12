ffce6d63b71d66b5e526e2296be4de13c12df419
doubleki7461 <doubleki437461@gmail.com>
2023-03-12 10:58:21 +0800

    创建了 烦人的幻灯片.cpp

---
+++ 许昌高中初级OJ/烦人的幻灯片.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Pos
+{
+    int xmax;
+    int xmin;
+    int ymax;
+    int ymin;
+    Pos()
+    {
+        // nothing
+    }
+    Pos(int __xmax, int __xmin, int __ymax, int __ymin) : xmax(__xmax), xmin(__xmin), ymax(__ymax), ymin(__ymin)
+    {
+        // nothing
+    }
+};
+int main()
+{
+    return 0;
+}
