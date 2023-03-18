22f1f8c39711933c4419abc91ba3a358590837c1
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-03-18 17:00:37 +0800

    格式化了 乘方.cpp

--- 许昌高中初级OJ/乘方.cpp
+++ 许昌高中初级OJ/乘方.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int pow_my(int a,int b)
+int pow_my(int a, int b)
 {
-    long long ans=pow(a,b);
-    long long inf=1e9;
-//    for(int i=1;i<=b;i++)
-//    {
-//        ans*=a;
-//        if(ans>inf)
-//        {
-//        	return -1;
-//		}
-//    }
-	if(ans>inf)
-	{
-		ans=-1;
-	}
+    long long ans = pow(a, b);
+    long long inf = 1e9;
+    //    for(int i=1;i<=b;i++)
+    //    {
+    //        ans*=a;
+    //        if(ans>inf)
+    //        {
+    //        	return -1;
+    //		}
+    //    }
+    if (ans > inf)
+    {
+        ans = -1;
+    }
     return ans;
 }
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<pow_my(a,b);
+    int a, b;
+    cin >> a >> b;
+    cout << pow_my(a, b);
     return 0;
 }
