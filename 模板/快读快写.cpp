49809cd748b886a78cd5b18c35e1282ea85e471c
doubleki7461 <doubleki437461@gmail.com>
2023-03-18 09:30:15 +0800

    更新了 快读快写.cpp

--- 模板/快读快写.cpp
+++ 模板/快读快写.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-char *p1, *p2, buf[100000];
-#define nc() (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++)
-inline int read()
+inline int readInt()
 {
     int x = 0, f = 1;
-    char ch = nc();
-    while (ch < 48 || ch > 57)
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
     {
         if (ch == '-')
         {
             f = -1;
         }
-        ch = nc();
+        ch = getchar();
     }
-    while (ch >= 48 && ch <= 57)
+    while (ch >= '0' && ch <= '9')
     {
-        x = x * 10 + ch - 48, ch = nc();
+        x = x * 10 + ch - '0';
+        ch = getchar();
     }
     return x * f;
 }
