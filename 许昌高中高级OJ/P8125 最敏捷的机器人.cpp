cc3afc1353bf2f4da9c112142a6e35a0667d2ecc
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-03-26 01:23:46 +0000

    创建了 最敏捷的机器人.cpp

---
+++ 许昌高中高级OJ/P8125 最敏捷的机器人.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1e6;
+const int MAXLOGN = 20;
+int n, k;
+int nums[MAXN + 5];
+int dmax[MAXN + 5][MAXLOGN + 5];
+int dmin[MAXN + 5][MAXLOGN + 5];
+inline int readInt()
+{
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+        {
+            f = -1;
+        }
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void write(int x)
+{
+    if (x < 0)
+    {
+        putchar('-'), x = -x;
+    }
+    if (x > 9)
+    {
+        write(x / 10);
+    }
+    putchar(x % 10 + '0');
+    return;
+}
+void init()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        dmax[i][0] = nums[i];
+        dmin[i][0] = nums[i];
+    }
+    for (int j = 1; (1 << j) <= n; j++)
+    {
+        for (int i = 1; i + (1 << j) - 1 <= n; i++)
+        {
+            dmax[i][j] = max(dmax[i][j - 1], dmax[i + (1 << (j - 1))][j - 1]);
+            dmin[i][j] = min(dmin[i][j - 1], dmin[i + (1 << (j - 1))][j - 1]);
+        }
+    }
+}
+int findmax(int l, int r)
+{
+    int k = 0;
+    while ((1 << (k + 1)) <= (r - l + 1))
+    {
+        k++;
+    }
+    return max(dmax[l][k], dmax[r - (1 << k) + 1][k]);
+}
+int findmin(int l, int r)
+{
+    int k = 0;
+    while ((1 << (k + 1)) <= (r - l + 1))
+    {
+        k++;
+    }
+    return min(dmin[l][k], dmin[r - (1 << k) + 1][k]);
+}
+int main()
+{
+    n = readInt();
+    k = readInt();
+    for (int i = 1; i <= n; i++)
+    {
+        nums[i] = readInt();
+    }
+    init();
+    for (int i = 1; i + k - 1 <= n; i++)
+    {
+        write(findmax(i, i + k - 1));
+        putchar(' ');
+        write(findmin(i, i + k - 1));
+        putchar('\n');
+    }
+    return 0;
+}
