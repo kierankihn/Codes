be27202ef9967bed7abe7ed26256f58f4be8919c
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-04-15 15:59:30 +0800

    Update P0289 【模板】扫描线.cpp

--- 许昌高中高级OJ/P0289 【模板】扫描线.cpp
+++ 许昌高中高级OJ/P0289 【模板】扫描线.cpp
@@ -20,7 +20,7 @@ inline int readInt()
     }
     return x * f;
 }
-inline void write(long long x)
+inline void write(ull x)
 {
     if (x < 0)
     {
@@ -34,10 +34,11 @@ inline void write(long long x)
     return;
 }
 
+typedef unsigned long long ull;
 struct Node
 {
-    unsigned long long cnt;
-    unsigned long long len;
+    ull cnt;
+    ull len;
     Node() : cnt(0), len(0)
     {
         // nothing
@@ -45,14 +46,14 @@ struct Node
 };
 struct Edge
 {
-    unsigned long long x;
-    pair<unsigned long long, unsigned long long> y;
-    unsigned long long k;
-    Edge() : x(0), y(pair<unsigned long long, unsigned long long>(0, 0)), k(0)
+    ull x;
+    pair<ull, ull> y;
+    ull k;
+    Edge() : x(0), y(pair<ull, ull>(0, 0)), k(0)
     {
         // nothing
     }
-    Edge(unsigned long long x_, unsigned long long y1_, unsigned long long y2_, unsigned long long k_) : x(x_), y(pair<unsigned long long, unsigned long long>(y1_, y2_)), k(k_)
+    Edge(ull x_, ull y1_, ull y2_, ull k_) : x(x_), y(pair<ull, ull>(y1_, y2_)), k(k_)
     {
         // nothing
     }
@@ -65,14 +66,14 @@ struct Edge
         return x < a.x;
     }
 };
-unsigned long long n;
-unsigned long long m;
-unsigned long long ans = 0;
+ull n;
+ull m;
+ull ans = 0;
 Node node[MAXN * 16];
-unsigned long long rk[MAXN * 2];
-unsigned long long raw[MAXN * 2];
+ull rk[MAXN * 2];
+ull raw[MAXN * 2];
 Edge edge[MAXN * 2];
-void updatelen(unsigned long long o, unsigned long long l, unsigned long long r)
+void updatelen(ull o, ull l, ull r)
 {
     if (node[o].cnt)
     {
@@ -83,7 +84,7 @@ void updatelen(unsigned long long o, unsigned long long l, unsigned long long r)
         node[o].len = node[o * 2].len + node[o * 2 + 1].len;
     }
 }
-void update(unsigned long long o, unsigned long long l, unsigned long long r, unsigned long long ql, unsigned long long qr, unsigned long long val)
+void update(ull o, ull l, ull r, ull ql, ull qr, ull val)
 {
     if (ql <= l && r <= qr)
     {
@@ -91,7 +92,7 @@ void update(unsigned long long o, unsigned long long l, unsigned long long r, un
         updatelen(o, l, r);
         return;
     }
-    unsigned long long mid = (l + r) >> 1;
+    ull mid = (l + r) >> 1;
     if (mid >= ql)
     {
         update(o * 2, l, mid, ql, qr, val);
@@ -107,9 +108,9 @@ int main()
     memset(rk, 0, sizeof(rk));
     memset(raw, 0, sizeof(raw));
     n = readInt();
-    for (unsigned long long i = 1; i <= n; i++)
+    for (ull i = 1; i <= n; i++)
     {
-        pair<unsigned long long, unsigned long long> x, y;
+        pair<ull, ull> x, y;
         x.first = readInt();
         y.first = readInt();
         x.second = readInt();
@@ -121,9 +122,9 @@ int main()
     }
     sort(rk + 1, rk + 2 * n + 1);
     m = unique(rk + 1, rk + 2 * n + 1) - rk - 1;
-    for (unsigned long long i = 1; i <= 2 * n; i++)
+    for (ull i = 1; i <= 2 * n; i++)
     {
-        pair<unsigned long long, unsigned long long> newy;
+        pair<ull, ull> newy;
         newy.first = lower_bound(rk + 1, rk + 2 * n + 1, edge[i].y.first) - rk;
         newy.second = lower_bound(rk + 1, rk + 2 * n + 1, edge[i].y.second) - rk;
         raw[newy.first] = edge[i].y.first;
@@ -131,7 +132,7 @@ int main()
         edge[i].y = newy;
     }
     sort(edge + 1, edge + 2 * n + 1);
-    for (unsigned long long i = 1; i < (n << 1); i++)
+    for (ull i = 1; i < (n << 1); i++)
     {
         update(1, 1, m, edge[i].y.first, edge[i].y.second - 1, edge[i].k);
         ans += (long long)node[1].len * (edge[i + 1].x - edge[i].x);
