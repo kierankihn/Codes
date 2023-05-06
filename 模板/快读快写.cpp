aa28d67911808b83aa55c01064b912d9ee4ccc21
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-05-06 10:19:13 +0800

    Update 快读快写.cpp

--- 模板/快读快写.cpp
+++ 模板/快读快写.cpp
@@ -1,29 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <typename T> T read()
+template <typename T> void read(T& x)
 {
 	T res = 0, s = 1;
 	char ch;
 	while ((ch = getchar()) < '0' || ch > '9')    if (ch == '-')    s = -1;
 	while (ch >= '0' && ch <= '9')                res = res * 10 + ch - '0', ch = getchar();
-	return res * s;
+	x = res * s;
 }
-template <typename T> void write(T x)
+template <typename First, typename... Rest> void read(First& first, Rest&... rest)
+{
+	read(first);
+	read(rest...);
+}
+void write()
+{
+	putchar('\n');
+}
+void write(char ch)
+{
+	putchar(ch);
+}
+template <typename T> void write(char delimiter, T x)
 {
 	if (x < 0)    putchar('-'), x = -x;
-	if (x > 9)    write(x / 10);
+	if (x > 9)    write(delimiter, x / 10);
 	putchar((x % 10) + '0');
+}
+template <typename First, typename... Rest> void write(char delimiter, First& first, Rest&... rest)
+{
+	write(delimiter, first);
+	putchar(delimiter);
+	if (sizeof...(rest) >= 1)
+	{
+		write(delimiter, rest...);
+	}
+}
+template <typename First, typename... Rest> void write(char delimiter, char linedelimiter, First& first, Rest&... rest)
+{
+	write(delimiter, first);
+	putchar(delimiter);
+	if (sizeof...(rest) >= 1)
+	{
+		write(delimiter, rest...);
+	}
+	putchar(linedelimiter);
+}
+template <typename... T> void write(T... t)
+{
+	write(' ', '\n', t...);
 }
 int main()
 {
-	int n;
-	n = read<int>();
-	for (int i = 1; i <= n; i++)
-	{
-		int x;
-		x = read<int>();
-		write(x);
-		putchar('\n');
-	}
+	int nums[785];
+	read(5, nums);
+	write(3, nums);
 	return 0;
 }
