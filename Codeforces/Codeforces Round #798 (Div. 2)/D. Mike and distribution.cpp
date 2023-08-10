5206cc2e453efeb76313c9a65aac5a7506054b63
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-08-10 07:40:02 +0100

    创建了 CF798 D. Mike and distribution.cpp

---
+++ Codeforces/Codeforces Round #798 (Div. 2)/D. Mike and distribution.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+namespace input
+{
+    template <typename T>
+    void read(T &x)
+    {
+        T res = 0, s = 1;
+        char ch;
+        while ((ch = getchar()) < '0' || ch > '9')
+            if (ch == '-')
+                s = -1;
+        while (ch >= '0' && ch <= '9')
+            res = res * 10 + ch - '0', ch = getchar();
+        x = res * s;
+    }
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        read(first);
+        read(rest...);
+    }
+}
+namespace output
+{
+    void write()
+    {
+        putchar('\n');
+    }
+    void write(char ch)
+    {
+        putchar(ch);
+    }
+    template <typename T>
+    void write(char delimiter, T x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            write(delimiter, x / 10);
+        putchar((x % 10) + '0');
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, char linedelimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+        putchar(linedelimiter);
+    }
+    template <typename... T>
+    void write(T... t)
+    {
+        write(' ', '\n', t...);
+    }
+}
+namespace solution
+{
+    typedef long long LL;
+    const int MAXN = 1e5 + 5;
+    LL n;
+    LL cnt;
+    LL suma, sumb;
+    struct Node
+    {
+        LL id, a, b;
+        Node() {}
+        Node(LL id_, LL a_, LL b_) : id(id_), a(a_), b(b_) {}
+    };
+    Node node[MAXN];
+    bool operator<(const Node a, const Node b)
+    {
+        if (a.a == b.a)
+        {
+            return a.b < b.b;
+        }
+        return a.a < b.a;
+    }
+    int main()
+    {
+        using input::read;
+        using output::write;
+        read(n);
+        for (int i = 1; i <= n; i++)
+        {
+            node[i].id = i;
+            read(node[i].a);
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            read(node[i].b);
+        }
+        std::sort(node + 1, node + n + 1);
+        std::cout << n / 2 + 1 << std::endl;
+        std::cout << node[n].id << " ";
+        for (int i = n - 1; i >= 1; i -= 2)
+        {
+            if (node[i].b > node[i - 1].b)
+            {
+                std::cout << node[i].id << " ";
+            }
+            else
+            {
+                std::cout << node[i - 1].id << " ";
+            }
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
