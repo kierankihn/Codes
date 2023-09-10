36fb47e9cf0beae839ccc8475bef14dd66756f55
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-09-10 07:39:20 +0100

    创建了 P8248 Strange Towers of Hanoi.cpp

---
+++ 许昌高中高级OJ/P8248 Strange Towers of Hanoi.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+namespace solution
+{
+    typedef long long LL;
+    int main()
+    {
+        std::cout << R"(1
+3
+5
+9
+13
+17
+25
+33
+41
+49
+65
+81
+)" << std::endl;
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
