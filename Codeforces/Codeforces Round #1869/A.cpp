cd4e861c11110edaae24a49cbaa6d0f4ff800ce9
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-09-11 00:27:02 +0100

    提交了 CF1869

---
+++ Codeforces/Codeforces Round #1869/A.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+namespace input
+{
+    template <typename T>
+    void read(T &x)
+    {
+        T res = 0, s = 1;
+        char ch;
+        while ((ch = getchar()) < '0' || ch > '9')
+            if (ch == '-')
+                s = -1;
+        while (ch >= '0' && ch <= '9')
+            res = res * 10 + ch - '0', ch = getchar();
+        x = res * s;
+    }
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        read(first);
+        read(rest...);
+    }
+}
+namespace output
+{
+    void write()
+    {
+        putchar('\n');
+    }
+    void write(char ch)
+    {
+        putchar(ch);
+    }
+    template <typename T>
+    void write(char delimiter, T x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            write(delimiter, x / 10);
+        putchar((x % 10) + '0');
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, char linedelimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+        putchar(linedelimiter);
+    }
+    template <typename... T>
+    void write(T... t)
+    {
+        write(' ', '\n', t...);
+    }
+}
+namespace solution
+{
+    typedef long long LL;
+    using input::read;
+    using output::write;
+    int main()
+    {
+        
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
