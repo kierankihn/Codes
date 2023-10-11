3881c4d96410e78140c0ba900c655ed5b351e0c3
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-10-11 15:46:04 +0800

    开始了 P8395 車的放置

---
+++ 许昌高中高级OJ/P8395 車的放置.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+namespace solution
+{
+    typedef long long LL;
+    int main()
+    {
+        
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
