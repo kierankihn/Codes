781f7c41e400498eba66e6c5a403ae43ee07697f
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-10-31 09:38:31 +0800

    updated 任务安排

--- 许昌高中高级OJ/P8189 任务安排1.cpp
+++ 许昌高中高级OJ/P8189 任务安排1.cpp
@@ -1,99 +1,35 @@
 #include <bits/stdc++.h>
-using namespace std;
-const int MAXN = 1e4 + 5;
-typedef long long LL;
-template <typename T> void read(T& x)
+namespace solution
 {
-	T res = 0, s = 1;
-	char ch;
-	while ((ch = getchar()) < '0' || ch > '9')    if (ch == '-')    s = -1;
-	while (ch >= '0' && ch <= '9')                res = res * 10 + ch - '0', ch = getchar();
-	x = res * s;
-}
-template <typename First, typename... Rest> void read(First& first, Rest&... rest)
-{
-	read(first);
-	read(rest...);
-}
-void write()
-{
-	putchar('\n');
-}
-void write(char ch)
-{
-	putchar(ch);
-}
-template <typename T> void write(char delimiter, T x)
-{
-	if (x < 0)    putchar('-'), x = -x;
-	if (x > 9)    write(delimiter, x / 10);
-	putchar((x % 10) + '0');
-}
-template <typename First, typename... Rest> void write(char delimiter, First& first, Rest&... rest)
-{
-	write(delimiter, first);
-	putchar(delimiter);
-	if (sizeof...(rest) >= 1)
-	{
-		write(delimiter, rest...);
-	}
-}
-template <typename First, typename... Rest> void write(char delimiter, char linedelimiter, First& first, Rest&... rest)
-{
-	write(delimiter, first);
-	putchar(delimiter);
-	if (sizeof...(rest) >= 1)
-	{
-		write(delimiter, rest...);
-	}
-	putchar(linedelimiter);
-}
-template <typename... T> void write(T... t)
-{
-	write(' ', '\n', t...);
-}
-LL n, s;
-LL c[MAXN];
-LL t[MAXN];
-double dp[MAXN];
-double sumc[MAXN], sumt[MAXN];
-deque<LL> q;
-inline double x(int i)
-{
-	return sumc[i];
-}
-inline double y(int i)
-{
-	return dp[i];
-}
-inline double slope(int i, int j)
-{
-	return (y(j) - y(i)) / (x(j) - x(i));
+    typedef long long LL;
+    const int MAXN = 1e5 + 5;
+    int n, s;
+    LL t[MAXN], c[MAXN];
+    LL sumt[MAXN], sumc[MAXN];
+    LL dp[MAXN];
+    int main()
+    {
+        std::memset(dp, 0x3f, sizeof(dp));
+        dp[0] = 0;
+        scanf("%d%d", &n, &s);
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%lld%lld", &t[i], &c[i]);
+            sumt[i] = sumt[i - 1] + t[i], sumc[i] = sumc[i - 1] + c[i];
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                dp[i] = std::min(dp[i], dp[j - 1] + sumt[i] * (sumc[i] - sumc[j - 1]) + s * (sumc[n] - sumc[j - 1]));
+            }
+        }
+        printf("%lld\n", dp[n]);
+        return 0;
+    }
 }
 int main()
 {
-	read(n, s);
-	for (int i = 1; i <= n; i++)
-	{
-		read(t[i], c[i]);
-		sumc[i] = sumc[i - 1] + c[i];
-		sumt[i] = sumt[i - 1] + t[i];
-	}
-	q.push_back(0);
-	for (int i = 1; i <= n; i++)
-	{
-		while (q.size() >= 2 && slope(q[0], q[1]) < s + sumt[i])
-		{
-			q.pop_front();
-		}
-		int j = q[0];
-		dp[i] = dp[j] - sumc[j] * (s + sumt[i]) + s * sumc[n] + sumc[i] * sumt[i];
-		while (q.size() >= 2 && slope(q[q.size() - 2], q[q.size() - 1]) > slope(q[q.size() - 1], i))
-		{
-			q.pop_back();
-		}
-		q.push_back(i);
-	}
-	write(dp[n]);
-	return 0;
+    solution::main();
+    return 0;
 }
--- 许昌高中高级OJ/P8190 任务安排2.cpp
+++ 许昌高中高级OJ/P8190 任务安排2.cpp
@@ -1,99 +1,53 @@
 #include <bits/stdc++.h>
-using namespace std;
-const int MAXN = 3e5 + 5;
-typedef long long LL;
-template <typename T> void read(T& x)
+namespace solution
 {
-	T res = 0, s = 1;
-	char ch;
-	while ((ch = getchar()) < '0' || ch > '9')    if (ch == '-')    s = -1;
-	while (ch >= '0' && ch <= '9')                res = res * 10 + ch - '0', ch = getchar();
-	x = res * s;
-}
-template <typename First, typename... Rest> void read(First& first, Rest&... rest)
-{
-	read(first);
-	read(rest...);
-}
-void write()
-{
-	putchar('\n');
-}
-void write(char ch)
-{
-	putchar(ch);
-}
-template <typename T> void write(char delimiter, T x)
-{
-	if (x < 0)    putchar('-'), x = -x;
-	if (x > 9)    write(delimiter, x / 10);
-	putchar((x % 10) + '0');
-}
-template <typename First, typename... Rest> void write(char delimiter, First& first, Rest&... rest)
-{
-	write(delimiter, first);
-	putchar(delimiter);
-	if (sizeof...(rest) >= 1)
-	{
-		write(delimiter, rest...);
-	}
-}
-template <typename First, typename... Rest> void write(char delimiter, char linedelimiter, First& first, Rest&... rest)
-{
-	write(delimiter, first);
-	putchar(delimiter);
-	if (sizeof...(rest) >= 1)
-	{
-		write(delimiter, rest...);
-	}
-	putchar(linedelimiter);
-}
-template <typename... T> void write(T... t)
-{
-	write(' ', '\n', t...);
-}
-LL n, s;
-LL c[MAXN];
-LL t[MAXN];
-LL dp[MAXN];
-LL sumc[MAXN], sumt[MAXN];
-deque<LL> q;
-inline double x(int i)
-{
-	return sumc[i];
-}
-inline double y(int i)
-{
-	return dp[i];
-}
-inline double slope(int i, int j)
-{
-	return (y(j) - y(i)) / (x(j) - x(i));
+    typedef long long LL;
+    const int MAXN = 3e5 + 5;
+    int n, s;
+    LL t[MAXN], c[MAXN];
+    LL sumt[MAXN], sumc[MAXN];
+    LL dp[MAXN];
+    struct Point
+    {
+        LL x, y;
+        Point(LL _x = 0, LL _y = 0) : x(_x), y(_y) {}
+    };
+    std::deque<Point> q;
+    double slope(Point x, Point y)
+    {
+        return (double)(x.y - y.y) / (x.x - y.x);
+    }
+    int main()
+    {
+        scanf("%d%d", &n, &s);
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%lld%lld", &t[i], &c[i]);
+            sumt[i] = sumt[i - 1] + t[i], sumc[i] = sumc[i - 1] + c[i];
+        }
+        q.push_back(Point(0, 0));
+        for (int i = 1; i <= n; i++)
+        {
+            while (q.size() >= 2 && slope(q[0], q[1]) < sumt[i] + s)
+            {
+                q.pop_front();
+            }
+            if (!q.empty())
+            {
+                dp[i] = q.front().y + sumt[i] * sumc[i] - (sumt[i] + s) * q.front().x + s * sumc[n];
+            }
+            while (q.size() >= 2 && slope(Point(sumc[i], dp[i]), q[q.size() - 1]) < slope(q[q.size() - 1], q[q.size() - 2]))
+            {
+                q.pop_back();
+            }
+            q.push_back(Point(sumc[i], dp[i]));
+        }
+        printf("%lld\n", dp[n]);
+        return 0;
+    }
 }
 int main()
 {
-	read(n, s);
-	for (int i = 1; i <= n; i++)
-	{
-		read(t[i], c[i]);
-		sumc[i] = sumc[i - 1] + c[i];
-		sumt[i] = sumt[i - 1] + t[i];
-	}
-	q.push_back(0);
-	for (int i = 1; i <= n; i++)
-	{
-		while (q.size() >= 2 && slope(q[0], q[1]) < s + sumt[i])
-		{
-			q.pop_front();
-		}
-		int j = q[0];
-		dp[i] = dp[j] - sumc[j] * (s + sumt[i]) + s * sumc[n] + sumc[i] * sumt[i];
-		while (q.size() >= 2 && slope(q[q.size() - 2], q[q.size() - 1]) > slope(q[q.size() - 1], i))
-		{
-			q.pop_back();
-		}
-		q.push_back(i);
-	}
-	write(dp[n]);
-	return 0;
+    solution::main();
+    return 0;
 }
