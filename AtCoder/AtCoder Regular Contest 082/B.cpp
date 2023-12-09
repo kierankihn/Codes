53f163faa419bea774884bdf52425c9fc8e9f5dd
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-09 11:26:55 +0800

    problem: created ARC082B

---
+++ AtCoder/AtCoder Regular Contest 082/B.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+namespace input
+{
+    template <typename T>
+    void read(T &x)
+    {
+        T res = 0, s = 1;
+        char ch;
+        while ((ch = getchar()) < '0' || ch > '9')
+            if (ch == '-')
+                s = -1;
+        while (ch >= '0' && ch <= '9')
+            res = res * 10 + ch - '0', ch = getchar();
+        x = res * s;
+    }
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        read(first);
+        read(rest...);
+    }
+}
+namespace output
+{
+    void write()
+    {
+        putchar('\n');
+    }
+    void write(char ch)
+    {
+        putchar(ch);
+    }
+    template <typename T>
+    void write(char delimiter, T x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            write(delimiter, x / 10);
+        putchar((x % 10) + '0');
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+    }
+    template <typename First, typename... Rest>
+    void write(char delimiter, char linedelimiter, First &first, Rest &...rest)
+    {
+        write(delimiter, first);
+        putchar(delimiter);
+        if (sizeof...(rest) >= 1)
+        {
+            write(delimiter, rest...);
+        }
+        putchar(linedelimiter);
+    }
+    template <typename... T>
+    void write(T... t)
+    {
+        write(' ', '\n', t...);
+    }
+}
+namespace solution
+{
+    typedef long long LL;
+    const int MAXN = 1e5 + 5;
+    using input::read;
+    using output::write;
+    int n;
+    int ans = 0;
+    int p[MAXN];
+    int main()
+    {
+        read(n);
+        for (int i = 1; i <= n; i++)
+        {
+            read(p[i]);
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (p[i] == i)
+            {
+                ans++;
+                std::swap(p[i], p[i + 1]);
+            }
+        }
+        write(ans);
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
