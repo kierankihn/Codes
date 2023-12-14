9c3d5145bc3980eb5c025a8e836baf8398173df1
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-14 12:41:01 +0800

    problem: created P7772

---
+++ 洛谷/P7772 [COCI2009-2010#2] FAKTOR.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+namespace solution
+{
+    typedef long long i64;
+    i64 a, i;
+    int main()
+    {
+        scanf("%lld%lld", &a, &i);
+        printf("%lld\n", a * (i - 1) + 1);
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
