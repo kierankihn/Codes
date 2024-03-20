ad27b3c415c3dc28e8401cd5c8be520810d032b8
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2024-03-20 08:00:30 +0800

    feat: :tada: created 'Stall Reservation'
    created XCGZOJ P1651 / XCGZOJ P8263 / Luogu P2859 / POJ P3190
    
    closed #69

---
+++ 许昌高中高级OJ/P1651 [USACO06FEB] Stall Reservations.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+namespace IO
+{
+    namespace basic_IO
+    {
+        template <typename T>
+        void __read(T &x)
+        {
+            char ch;
+            T res = 0, f = 1;
+            while (std::isdigit(ch = getchar()) == false)
+            {
+                f = (ch != '-');
+            }
+            do
+            {
+                res = res * 10 + (ch - '0');
+            } while (std::isdigit(ch = getchar()) == true);
+            x = res * (f ? 1 : -1);
+        }
+        template <typename First, typename... Rest>
+        void __read(First &first, Rest &...rest)
+        {
+            __read(first);
+            __read(rest...);
+        }
+        template <typename T>
+        void __write(T x)
+        {
+            if (x < 0)
+            {
+                std::putchar('-'), __write(-x);
+                return;
+            }
+            if (x >= 10)
+            {
+                __write(x / 10);
+            }
+            std::putchar(x % 10 + '0');
+        }
+        template <typename First, typename... Rest>
+        void __write(First &first, Rest &...rest)
+        {
+            __write(first), std::putchar(' '), __write(rest...);
+        }
+    }
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        basic_IO::__read(first, rest...);
+    }
+    template <typename T>
+    void readAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__read(*i);
+        }
+    }
+    template <typename First, typename... Rest>
+    void write(First first, Rest... rest)
+    {
+        basic_IO::__write(first, rest...), std::putchar('\n');
+    }
+    template <typename T>
+    void writeAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__write(*i);
+        }
+    }
+}
+namespace solution
+{
+    using namespace IO;
+    typedef long long i64;
+    typedef std::pair<int, int> pii;
+    typedef std::multiset<int, std::greater<int>> msi;
+    const int MAXN = 5e4 + 5;
+    int n;
+    pii a[MAXN];
+    msi s;
+    int main()
+    {
+        read(n);
+        for (int i = 1; i <= n; i++)
+        {
+            read(a[i].first, a[i].second);
+        }
+        std::sort(a + 1, a + n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            auto it = s.lower_bound(a[i].first);
+            if (it != s.end())
+            {
+                s.erase(it);
+            }
+            s.insert(a[i].second + 1);
+        }
+        write(s.size());
+        return 0;
+    }
+}
+int main()
+{
+    solution::main();
+    return 0;
+}
