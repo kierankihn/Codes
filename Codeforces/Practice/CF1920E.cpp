d3654e0cc8bf6414f372f2456732a6d830af2bd9
kierankihn <114803508+kierankihn@users.noreply.github.com>
2024-05-28 04:18:44 +0100

    feat: :sparkles: create CF practice

---
+++ Codeforces/Practice/CF1920E.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+namespace IO
+{
+    namespace basic_IO
+    {
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __read(T &x)
+        {
+            char ch;
+            T res = 0, f = 1;
+            while (std::isdigit(ch = getchar()) == false)
+            {
+                f = (ch != '-');
+            }
+            do
+            {
+                res = res * 10 + (ch - '0');
+            } while (std::isdigit(ch = getchar()) == true);
+            x = res * (f ? 1 : -1);
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __read(T &x)
+        {
+            std::cin >> x;
+        }
+        template <typename First, typename... Rest>
+        void __read(First &first, Rest &...rest)
+        {
+            __read(first);
+            __read(rest...);
+        }
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __write(T x)
+        {
+            if (x < 0)
+            {
+                std::putchar('-'), __write(-x);
+                return;
+            }
+            if (x >= 10)
+            {
+                __write(x / 10);
+            }
+            std::putchar(x % 10 + '0');
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __write(T x)
+        {
+            std::cout << x;
+        }
+        template <typename First, typename... Rest>
+        void __write(First first, Rest... rest)
+        {
+            __write(first), std::putchar(' '), __write(rest...);
+        }
+    }  // namespace basic_IO
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        basic_IO::__read(first, rest...);
+    }
+    template <typename T>
+    void readAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__read(*i);
+        }
+    }
+    long long readi()
+    {
+        long long res;
+        read(res);
+        return res;
+    }
+    std::string reads()
+    {
+        std::string res;
+        read(res);
+        return res;
+    }
+    template <typename First, typename... Rest>
+    void write(First first, Rest... rest)
+    {
+        basic_IO::__write(first, rest...), std::putchar('\n');
+    }
+    template <typename T>
+    void writeAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__write(*i), std::putchar(' ');
+        }
+        std::putchar('\n');
+    }
+}  // namespace IO
+namespace solution
+{
+    using namespace IO;
+    typedef long long i64;
+
+    constexpr int MAXN = 3e3 + 5;
+    constexpr int MOD  = 998244353;
+
+    int t;
+    int n, m;
+
+    i64 ans = 0;
+
+    i64 dp[MAXN][MAXN];
+
+    i64 solve(int i, int j)
+    {
+        if (i == 0)
+        {
+            return 1;
+        }
+        if (dp[i][j])
+        {
+            return dp[i][j];
+        }
+
+        for (int k = 1; j + k - 1 <= m && j * k <= i; k++)
+        {
+            dp[i][j] += solve(i - j * k, k), dp[i][j] %= MOD;
+        }
+
+        return dp[i][j];
+    }
+
+    int main()
+    {
+        read(t);
+        while (t--)
+        {
+            read(n, m);
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    dp[i][j] = 0;
+                }
+            }
+
+            ans = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                ans += solve(n, i), ans %= MOD;
+            }
+
+            write(ans);
+        }
+
+        return 0;
+    }
+}  // namespace solution
+int main()
+{
+    solution::main();
+    return 0;
+}
+
+/**
+ * dp[i][j]: 贡献为 i，两个 1 之间的距离为 j
+ * dp[i][j] -> dp[i + j * k][k]
+ */
