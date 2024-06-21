5fb31a6afa9c6d9de85280a0a15a73b2bc139085
kierankihn <114803508+kierankihn@users.noreply.github.com>
2024-06-21 07:41:19 +0100

    feat: :sparkles: created contest 20240621

---
+++ Contest/20240621/T1/T1.cpp
@@ -0,0 +1,175 @@
+/**
+ * @author Kieran Kihn
+ * @date 2024.06.21
+ * @name CF1027E
+ * @link https://www.luogu.com.cn/problem/CF1027E
+ */
+#include <bits/stdc++.h>
+namespace IO
+{
+    namespace basic_IO
+    {
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __read(T &x)
+        {
+            char ch;
+            T res = 0, f = 1;
+            while (std::isdigit(ch = getchar()) == false)
+            {
+                f = (ch != '-');
+            }
+            do
+            {
+                res = res * 10 + (ch - '0');
+            } while (std::isdigit(ch = getchar()) == true);
+            x = res * (f ? 1 : -1);
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __read(T &x)
+        {
+            std::cin >> x;
+        }
+        template <typename First, typename... Rest>
+        void __read(First &first, Rest &...rest)
+        {
+            __read(first);
+            __read(rest...);
+        }
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __write(T x)
+        {
+            if (x < 0)
+            {
+                std::putchar('-'), __write(-x);
+                return;
+            }
+            if (x >= 10)
+            {
+                __write(x / 10);
+            }
+            std::putchar(x % 10 + '0');
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __write(T x)
+        {
+            std::cout << x;
+        }
+        template <typename First, typename... Rest>
+        void __write(First first, Rest... rest)
+        {
+            __write(first), std::putchar(' '), __write(rest...);
+        }
+    }  // namespace basic_IO
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        basic_IO::__read(first, rest...);
+    }
+    template <typename T>
+    void readAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__read(*i);
+        }
+    }
+    long long readi()
+    {
+        long long res;
+        read(res);
+        return res;
+    }
+    std::string reads()
+    {
+        std::string res;
+        read(res);
+        return res;
+    }
+    template <typename First, typename... Rest>
+    void write(First first, Rest... rest)
+    {
+        basic_IO::__write(first, rest...), std::putchar('\n');
+    }
+    template <typename T>
+    void writeAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__write(*i), std::putchar(' ');
+        }
+        std::putchar('\n');
+    }
+}  // namespace IO
+namespace solution
+{
+    using namespace IO;
+    typedef long long i64;
+
+    constexpr int MAXN = 500 + 5;
+    constexpr int MOD  = 998244353;
+
+    int n, k;
+
+    int ans = 0;
+
+    int sum[MAXN][MAXN];
+
+    int dp[MAXN][MAXN];
+
+    int main()
+    {
+        read(n, k);
+
+        sum[0][0] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                for (int p = 0; p < j; p++)
+                {
+                    sum[i][j] = (sum[i][j] + sum[i - j][p]) % MOD;
+                }
+                for (int p = 1; p <= j; p++)
+                {
+                    sum[i][j] = (sum[i][j] + sum[i - p][j]) % MOD;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                sum[i][j] = sum[i][j] * 2 % MOD;
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                for (int p = 1; p <= i && j * p < k; p++)
+                {
+                    dp[i][j] = (dp[i][j] + (p != i ? dp[i - p][j] : sum[n][j])) % MOD;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            ans += dp[n][i], ans %= MOD;
+        }
+        write(ans);
+
+        return 0;
+    }
+}  // namespace solution
+int main()
+{
+    solution::main();
+    return 0;
+}
+
+/**
+ * sum[i][j] <- sum[i - j][p]
+ * sum[i][j] <- sum[i - p][j]
+ */
