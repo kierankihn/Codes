fc078ead168ba7ef83c96ad819ac340c68c80aa1
kierankihn <114803508+kierankihn@users.noreply.github.com>
2024-07-04 19:10:06 +0800

    feat: :sparkles: created contest 20240703

---
+++ Contest/20240703/T6/T6.cpp
@@ -0,0 +1,145 @@
+/**
+ * @author Kieran Kihn
+ * @date 2024.07.03
+ * @name T6
+ * @link
+ */
+#include <bits/stdc++.h>
+namespace IO
+{
+    namespace basic_IO
+    {
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __read(T &x)
+        {
+            char ch;
+            T res = 0, f = 1;
+            while (std::isdigit(ch = getchar()) == false)
+            {
+                f = (ch != '-');
+            }
+            do
+            {
+                res = res * 10 + (ch - '0');
+            } while (std::isdigit(ch = getchar()) == true);
+            x = res * (f ? 1 : -1);
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __read(T &x)
+        {
+            std::cin >> x;
+        }
+        template <typename First, typename... Rest>
+        void __read(First &first, Rest &...rest)
+        {
+            __read(first);
+            __read(rest...);
+        }
+        template <typename T>
+        typename std::enable_if<std::is_integral<T>::value>::type __write(T x)
+        {
+            if (x < 0)
+            {
+                std::putchar('-'), __write(-x);
+                return;
+            }
+            if (x >= 10)
+            {
+                __write(x / 10);
+            }
+            std::putchar(x % 10 + '0');
+        }
+        template <typename T>
+        typename std::enable_if<!std::is_integral<T>::value>::type __write(T x)
+        {
+            std::cout << x;
+        }
+        template <typename First, typename... Rest>
+        void __write(First first, Rest... rest)
+        {
+            __write(first), std::putchar(' '), __write(rest...);
+        }
+    }  // namespace basic_IO
+    template <typename First, typename... Rest>
+    void read(First &first, Rest &...rest)
+    {
+        basic_IO::__read(first, rest...);
+    }
+    template <typename T>
+    void readAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__read(*i);
+        }
+    }
+    long long readi()
+    {
+        long long res;
+        read(res);
+        return res;
+    }
+    std::string reads()
+    {
+        std::string res;
+        read(res);
+        return res;
+    }
+    template <typename First, typename... Rest>
+    void write(First first, Rest... rest)
+    {
+        basic_IO::__write(first, rest...), std::putchar('\n');
+    }
+    template <typename T>
+    void writeAll(T *begin, T *end)
+    {
+        for (T *i = begin; i != end; i++)
+        {
+            basic_IO::__write(*i), std::putchar(' ');
+        }
+        std::putchar('\n');
+    }
+}  // namespace IO
+namespace solution
+{
+    using namespace IO;
+    typedef long long i64;
+
+    constexpr int MAXN = 100 + 5;
+
+    i64 n;
+    i64 a[MAXN];
+
+    i64 gcd;
+
+    int main()
+    {
+        read(n);
+        for (int i = 1; i <= n; i++)
+        {
+            a[0] = std::max(a[0], a[i] = readi());
+        }
+
+        gcd = a[1];
+        for (int i = 2; i <= n; i++)
+        {
+            gcd = std::__gcd(gcd, a[i]);
+        }
+
+        if ((a[0] / gcd - n) & 1)
+        {
+            write("Alice");
+        }
+        else
+        {
+            write("Bob");
+        }
+
+        return 0;
+    }
+}  // namespace solution
+int main()
+{
+    solution::main();
+    return 0;
+}
